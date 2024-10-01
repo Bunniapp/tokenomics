f8137b4da6a8e9eb89d3b54dd1bac06fe5584114
ZeframLou <zeframlou@gmail.com>
2024-09-30 19:03:08 -0700

    feat: add MasterBunni deploy script

--- script/DeployMasterBunni.s.sol
+++ script/DeployMasterBunni.s.sol
@@ -2,23 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {CREATE3Script} from "./base/CREATE3Script.sol";
+import {IMasterBunni} from "../src/interfaces/IMasterBunni.sol";
+import {MasterBunni} from "../src/MasterBunni.sol";
 
 contract DeployScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() external {
+    function run() external returns (IMasterBunni masterBunni) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
         vm.startBroadcast(deployerPrivateKey);
 
-        /* Example CREATE3 deployment
-        uint256 param = 123;
-        c = Contract(
-            create3.deploy(
-                getCreate3ContractSalt("Contract"), bytes.concat(type(Contract).creationCode, abi.encode(param))
-            )
-        ); 
-        */
+        masterBunni = IMasterBunni(
+            create3.deploy(getCreate3ContractSalt("MasterBunni"), bytes.concat(type(MasterBunni).creationCode))
+        );
 
         vm.stopBroadcast();
     }
